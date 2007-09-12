eb5590fc37c1eb884b1592fcc51ef28ac0099795
lar <lar@1626b6dc-b897-4dde-b697-cfba1b6e6f61>
2007-09-12 16:54:16 +0000

    initial standfor layout/styles
    git-svn-id: https://sympact-standfor.svn.ey02.engineyard.com/src/trunk/standfor@303 1626b6dc-b897-4dde-b697-cfba1b6e6f61

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,59 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+#  def new_box(&block)
+#    b = markaby do 
+#      div.default.box do
+#        h2 "Testing Markaby Helper"
+#        p "testing"
+#        capture(&block)
+#      end
+#    end
+#    concat b.to_s, block.binding
+#  end
+#  
+#  def markaby(&block)
+#    Markaby::Builder.new({}, self, &block)
+#  end
+
+  def content_box(header_text, opts = {}, &block)
+    concat("<div " + (opts[:id] ? "id=\"#{opts[:id]}\" " : "") + "class=\"" + (opts[:class] ? "#{opts[:class]} box\"" : "default box\"") +">", block.binding )   
+    concat("<div class=\"content\">", block.binding)
+    concat("  <div class=\"t\"></div>", block.binding)
+    concat("  <h1>#{header_text}</h1>", block.binding)
+    yield
+    concat("</div>", block.binding)
+    concat("<div class=\"b\"><div></div></div>", block.binding)
+    concat("</div>", block.binding)
+  end
+  
+#  def markaby(&proc)
+#    assigns = {}
+#      instance_variables.each do |name|
+#        assigns[ name[1..-1] ] =  instance_variable_get(name)
+#      end
+#    Markaby::Builder.new(assigns, self).capture(&proc)
+#  end
+#  
+#  def tasks(&block)
+#    markaby do
+#      div.tasks {
+#        ul {
+#          markaby(&block)
+#        }
+#      }
+#    end
+#  end
+#  
+#  def contenter()
+#    tasks do
+#      task
+#    end
+#  end
+#  
+#  def task 
+#    return "hi there"
+#  end
+  
+  
 end
