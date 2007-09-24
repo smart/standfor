56b06fee20d6fc558c8d2d2724a3056ed85841f4
smartocci <smartocci@1626b6dc-b897-4dde-b697-cfba1b6e6f61>
2007-09-24 19:48:35 +0000

    can use sitename in routes now
    git-svn-id: https://sympact-standfor.svn.ey02.engineyard.com/src/trunk/standfor@367 1626b6dc-b897-4dde-b697-cfba1b6e6f61

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -4,4 +4,13 @@ class Organization < ActiveRecord::Base
   has_many :badges
   has_many :campaigns
   has_one :style_info, :as => :scope
+  
+  def self.find(*args)
+    return (args[0].is_a?(String) ? self.find_by_site_name(*args) : super )
+  end
+  
+  def to_param
+  "#{site_name}"
+  end
+  
 end
--- app/models/segment.rb
+++ app/models/segment.rb
@@ -5,4 +5,12 @@ class Segment < ActiveRecord::Base
    has_many :campaigns
    validates_presence_of :name, :keyword
    has_one :style_info, :as => :scope
+  
+  def self.find(*args)
+    return (args[0].is_a?(String) ? self.find_by_site_name(*args) : super )
+  end 
+   
+   def to_param
+     "#{site_name}"
+   end
 end
