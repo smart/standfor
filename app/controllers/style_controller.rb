ab5451435568cd7c31db05af7c83b09cb776a46a
smartocci <smartocci@1626b6dc-b897-4dde-b697-cfba1b6e6f61>
2007-09-25 21:29:03 +0000

    updated image tinting
    git-svn-id: https://sympact-standfor.svn.ey02.engineyard.com/src/trunk/standfor@384 1626b6dc-b897-4dde-b697-cfba1b6e6f61

--- app/controllers/style_controller.rb
+++ app/controllers/style_controller.rb
@@ -98,13 +98,15 @@ class StyleController < ApplicationController
  
   def color_image(base_image, color1 = "black")
     #begin
+     dark_color = darken_color(color1)
+     light_color = lighten_color(color1)
       list = Magick::ImageList.new
       image =  Magick::Image.read(base_image).first
       list << image
       gradient_list = Magick::ImageList.new
-      gradient1 = Magick::Image.read("gradient:black-#{color1} -resize 45") { self.size = "10x100" }
+      gradient1 = Magick::Image.read("gradient:#{dark_color}-#{color1} -resize 45") { self.size = "10x100" }
       gradient_list << gradient1.first
-      gradient2 = Magick::Image.read("gradient:#{color1}-snow -resize 45") { self.size = "10x100" }
+      gradient2 = Magick::Image.read("gradient:#{color1}-#{light_color} -resize 45") { self.size = "10x100" }
       gradient_list << gradient2.first
       list << gradient_list.append(true)
       return list.fx("v.p{0,u*v.h}")
@@ -112,5 +114,30 @@ class StyleController < ApplicationController
 #      return image =  Magick::Image.read(base_image).first
 #    end
   end
+  
+  def darken_color(color = "#FFFFFF", level = "88")
+    adjust_color(color, level, true)
+  end
+  
+  def lighten_color(color = "#000000", level = "55")
+    adjust_color(color, level, false)
+  end
+  
+  def adjust_color(color = "#FFFFFF", level = "CC", darken = true)
+   
+    adjust_by = level.to_i(base=16)
+    return_color = "#"
+    colors = [color[1,2].to_i(base=16), color[3,2].to_i(base=16), color[5,2].to_i(base=16) ]
+    colors.map do |color|
+      color =  (darken ? (color - adjust_by) : (color + adjust_by))
+      color = 0 if color < 0
+      color = 255 if color > 255
+      color = color.to_s(base=16)
+      color = "0" + color if color.length != 2
+      return_color << color
+    end
+    return return_color
+  end 
+    
 
 end
