6b5568d667eb4cd37250a908ca6648da0f0a0738
smartocci <smartocci@1626b6dc-b897-4dde-b697-cfba1b6e6f61>
2007-09-26 16:59:58 +0000

    moved these files to plugin
    git-svn-id: https://sympact-standfor.svn.ey02.engineyard.com/src/trunk/standfor@391 1626b6dc-b897-4dde-b697-cfba1b6e6f61

--- lib/adiserver_resource.rb
+++
@@ -1,6 +0,0 @@
-class AdiserverResource < ActiveResource::Base
- # replace with deployment url
-  self.site = "#{ADISERVER}" 
-  site.user = "#{ADISERVER_USER}" 
-  site.password = "#{ADISERVER_PASS}" 
-end
