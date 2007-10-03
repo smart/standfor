892fd22b057e5abbe5bf4cc3796872b2ac263317
elmo <elmo@1626b6dc-b897-4dde-b697-cfba1b6e6f61>
2007-10-03 20:45:15 +0000

    logos
    git-svn-id: https://sympact-standfor.svn.ey02.engineyard.com/src/trunk/standfor@431 1626b6dc-b897-4dde-b697-cfba1b6e6f61

--- app/models/organizationslogo.rb
+++ app/models/organizationslogo.rb
@@ -1,6 +1,7 @@
 class Organizationslogo < ActiveRecord::Base
   belongs_to :organization
-  acts_as_attachment :storage => :file_system, :content_type => :image, :thumbnails => { :normal => '300>', :thumb => '75' }
+  acts_as_attachment :storage => :file_system, 
+		     :content_type => :image, :thumbnails => { :normal => '300>', :thumb => '75' }
   validates_as_attachment
 
  def to_param
