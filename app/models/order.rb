7386a4be6789de8f13a2e6d7cfec0ef6ff775ccb
elmo <elmo@1626b6dc-b897-4dde-b697-cfba1b6e6f61>
2007-10-23 20:20:57 +0000

    checkpoint
    git-svn-id: https://sympact-standfor.svn.ey02.engineyard.com/src/trunk/standfor@458 1626b6dc-b897-4dde-b697-cfba1b6e6f61

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,13 @@
 class Order < ActiveRecord::Base
   belongs_to :account
   has_many :donations
+
+  def total
+    total = 0
+    self.donations.each do |donation| 
+       total += donation.amount
+    end
+    total
+  end
+
 end
