90325da78cf8921deb9306504a279e3281b956da
elmo <elmo@1626b6dc-b897-4dde-b697-cfba1b6e6f61>
2007-11-01 22:04:35 +0000

    checkpoing for regirstration
    git-svn-id: https://sympact-standfor.svn.ey02.engineyard.com/src/trunk/standfor@479 1626b6dc-b897-4dde-b697-cfba1b6e6f61

---
+++ app/controllers/worldreach/accounts_controller.rb
@@ -0,0 +1,38 @@
+class Worldreach::AccountsController < ApplicationController
+  # Be sure to include AuthenticationSystem in Application Controller instead
+  # If you want "remember me" functionality, add this before_filter to Application Controller
+  before_filter :login_from_cookie
+	layout 'worldreach/default'
+
+  include YouserSystem
+
+  # render new.rhtml
+  def new
+  end
+  
+  def finish_registration
+    @account = current_account
+  end
+  
+  def save_registration
+    @account = current_account
+    if current_account.update_attributes(params[:account])
+      redirect_back_or_default('/') and  return  false
+    else
+      render :action => 'finish_registration'
+    end
+  end
+
+  def create
+    @local_user = LocalUser.new(params[:account])
+
+    if @local_user.save!
+      successful_local_user_login(@local_user)
+      return false
+    else
+      render :action => "new"
+    end
+
+  end
+
+end
