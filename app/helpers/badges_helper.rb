574c9c549a99e1427f87bec9b5dcdeaf4f4a8555
lar <lar@1626b6dc-b897-4dde-b697-cfba1b6e6f61>
2007-11-09 19:33:52 +0000

    git-svn-id: https://sympact-standfor.svn.ey02.engineyard.com/src/trunk/standfor@526 1626b6dc-b897-4dde-b697-cfba1b6e6f61

--- app/helpers/badges_helper.rb
+++ app/helpers/badges_helper.rb
@@ -1,13 +1,18 @@
 module BadgesHelper
-  def badge_link(badge)
-  	link = link_to(image_tag("placeholders/imageselections.jpg"), badge_path(badge))
+  def badge_link(badge, path)
+  	link = link_to(image_tag("placeholders/imageselections.jpg"), path)
   	meta = content_tag(:li, badge.name)
   	meta << content_tag(:li, "Organization: " + badge.organization.name)
   	meta << content_tag(:li, "Cause: " + badge.segment.name)
   	meta << content_tag(:li, link_to("Get this Badge", badge_path(badge)))
   	meta = content_tag(:ul, meta)
-  	meta = content_tag(:div, meta, :class => 'badge-meta')
+  	meta = link + meta
+  	meta = content_tag(:div, meta, :class => 'badge-meta', :id => dom_id(badge))
     html = content_tag(:div, meta + link, :class => 'badge-link')
     html
   end
+  
+  def show_badge_meta(badge)
+  	
+	end
 end
