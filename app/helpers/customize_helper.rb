d8d7ac8b7793baa13af27cd6f8208fe40aa1c256
elmo <elmo@1626b6dc-b897-4dde-b697-cfba1b6e6f61>
2007-12-13 19:01:31 +0000

    changes for issue #51
    git-svn-id: https://sympact-standfor.svn.ey02.engineyard.com/src/trunk/standfor@674 1626b6dc-b897-4dde-b697-cfba1b6e6f61

--- app/helpers/customize_helper.rb
+++ app/helpers/customize_helper.rb
@@ -3,6 +3,11 @@ module CustomizeHelper
   def option_link(icon_class, text, my_badge)
 #    html = image_tag(image, :alt => text)
 #    html += text
+    css_classes = {
+      'picture.png' => 'images' ,
+      'font.png' => 'fonts' ,
+      'border.png' => 'edges' ,
+    }
     html = link_to_remote text, 
       :url => {:controller => "/customize",
       :action => "choose_customizable",
@@ -10,7 +15,7 @@ module CustomizeHelper
       :id => my_badge  },
       :before => "Element.show('indicator')",
       :complete => "Element.hide('indicator')"
-    html = content_tag(:li, html, :class => icon_class)
+    html = content_tag(:li, html, :class => css_classes[icon_class])
     return html
   end
   
