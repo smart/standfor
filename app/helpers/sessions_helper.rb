7ce048ed66180030ef2ea45f25dea42acd03ffa3
supaspoida <supaspoida@1626b6dc-b897-4dde-b697-cfba1b6e6f61>
2008-03-31 07:03:50 +0000

    Second sprint for view refactor [#145]
    git-svn-id: https://sympact-standfor.svn.ey02.engineyard.com/src/trunk/standfor@770 1626b6dc-b897-4dde-b697-cfba1b6e6f61

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,22 @@
 module SessionsHelper
+  def session_links
+    render(:partial => 'shared/patterns/session_links')
+  end
+  
+  def logout_link
+    link_to 'Logout', logout_path
+  end
+  
+  def login_link
+    link_to 'Login', login_path
+  end
+  
+  def signup_link
+    link_to 'Signup', signup_path
+  end
+  
+  def account_link
+    link_to 'My Account', user_account_path
+  end
+  
 end
